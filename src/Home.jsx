150a07834094d0492cc50e6bd771a6e833b60819
TommasoRodriguesdf <dtommasorodrigues@gmail.com>
2023-11-24 14:32:14 -0400

    griei o gitignore
    griei o gitignore

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,5 +1,12 @@
+import {useState} from 'react'
+import {useState} from 'react'
+import {useState} from 'react-react-'
+
+
 export default function Home(){
     return(
         <h1>PTAC 2 - PROJETO FINAL</h1>
+
+        
      );
 }
